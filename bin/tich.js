021841086a201c5e09bd4332a7b3af0d39a2372a
Cristina Avila <mcristina.mavila@gmail.com>
2018-08-27 10:15:07 -0500

    TiCh changes

--- bin/tich.js
+++ bin/tich.js
@@ -80,30 +80,36 @@ function tich() {
         var defaultIcon = './app/assets/iphone/iTunesArtwork@2x.png';
         console.log('update default icon')
 
-        copy({
-          files: {
-            'DefaultIcon.png': defaultIcon
-          },
-          dest: './',
-          overwrite: true
-        }, function (err) {
-            console.log('Updating DefaultIcon.png');
-        });
+        if(fs.existsSync(defaultIcon)){
+            copy({
+                files: {
+                  'DefaultIcon.png': defaultIcon
+                },
+                dest: './',
+                overwrite: true
+              }, function (err) {
+                  console.log('Updating DefaultIcon.png');
+              });
+        }
+
+        
 
 
         //Update LaunchLogo
         var defaultLaunchLogo = './app/assets/iphone/LaunchLogo.png';
         console.log('update default launch logo')
 
-        copy({
-          files: {
-            'LaunchLogo.png': defaultLaunchLogo
-          },
-          dest: './',
-          overwrite: true
-        }, function (err) {
-            console.log('Updating LaunchLogo.png');
-        });
+        if(fs.existsSync(defaultLaunchLogo)){
+            copy({
+                files: {
+                  'LaunchLogo.png': defaultLaunchLogo
+                },
+                dest: './',
+                overwrite: true
+              }, function (err) {
+                  console.log('Updating LaunchLogo.png');
+            });
+        }
 
         if( fs.existsSync('./notification_icon.sh') ){
             exec('./notification_icon.sh '+ alloyCfg.global.theme, function() {
@@ -350,7 +356,22 @@ function tich() {
 
                                 });
                             } else {
-                                removePlatformStoryBoard();
+                                var assetsDirectory = './app/themes/' + alloyCfg.global.theme + '/assets/'
+                                console.log('Start copy from ' + assetsDirectory)
+
+                                if( fs.existsSync(assetsDirectory) ){
+                                    ncp(assetsDirectory, './app/assets/', function (err) {
+                                        if (err) {
+                                            console.error(err);
+                                        }
+
+                                        console.log(chalk.cyan(alloyCfg.global.theme + ' assets copied'));
+                                        console.log('Start Platform Assets management')
+                                        removePlatformStoryBoard();
+                                    });
+                                } else {
+                                    removePlatformStoryBoard();
+                                }
                             }
                         });
 
@@ -378,6 +399,7 @@ function tich() {
         .option('-o, --out <path>', 'Specifies the file to write (default: tiapp.xml)')
         .option('-s, --select <name>', 'Updates TiApp.xml to config specified by <name>')
         .option('--noalloy', 'Do no update theme on Alloy config')
+        .option('--ticfg <path>', 'Restore config.json from another file')
         //.option('-c, --capture <name>', "Stores the current values of TiApp.xml id, name, version as <name> ")
 
     program.parse(process.argv);
@@ -386,6 +408,23 @@ function tich() {
     var infile = program.in ? program.in : './tiapp.xml';
     var outfile = program.out ? program.out : './tiapp.xml';
     var processAlloy = program.noalloy ? false : true;
+    var ticfg = program.ticfg ? true : false;
+
+
+    if( ticfg ){
+        if( fs.existsSync(program.ticfg) ){
+            copy({
+                files: {
+                    'config.json': program.ticfg
+                },
+                dest: './app/',
+                overwrite: true
+              }, function (err) {
+                  console.log('Replacing config.json');
+              });
+
+        }
+    }
 
     // check that all required input paths are good
     [cfgfile, infile].forEach(function (file) {
